0bc32c13db582b2d008c479b8ff4527eb77f9741
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2025-03-20 17:54:02 +0300

    fix

--- src/js/modules/team.js
+++ src/js/modules/team.js
@@ -16,7 +16,7 @@ if ($(".team").length) {
         
             if ($("#teamPageMobileNav").length !== 0) { return }
 
-            let nav = $("<div>", {id: "teamPageMobileNav", style: `background: white; border-bottom: 1px solid #D9D9D9; z-index: 9; display: flex; padding: 10px; gap: 10px; position: fixed; left: 0; right: 0; top: ${headerHeight}px`}),
+            let nav = $("<div>", {id: "teamPageMobileNav", style: `background: white; border-bottom: 1px solid #D9D9D9; z-index: 7; display: flex; padding: 10px; gap: 10px; position: fixed; left: 0; right: 0; top: ${headerHeight}px`}),
                 logo = $("<img>", {src: $(".team__logo img").attr("src"), width: "53px"}),
                 btn1 = $($(".team__links a")[0].cloneNode(true)).attr("style", "max-width: unset; flex: 1 1;"),
                 btn2 = $($(".team__links a")[1].cloneNode(true)).attr("style", "max-width: unset; flex: 1 1;")
