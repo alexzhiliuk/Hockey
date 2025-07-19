4d6639ba7e895ba496d0b94ce25e3a6d9c339f73
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2025-07-19 17:52:41 +0300

    fix

--- src/js/modules/tournament.js
+++ src/js/modules/tournament.js
@@ -2,23 +2,46 @@
 
 export function drawLine() {
     try {
+        
+        let gridHeight = $(".tournament-grid").innerHeight();
+        let thirdHeight = $(".tournament-card_3nd").innerHeight();
+
+        let bottom = (gridHeight - thirdHeight) / 2;
+
+        console.log(bottom)
+        bottom = bottom < 270 ? -1 * (bottom - 270) : 0;
+
+        console.log(bottom)
+        
+        if (thirdHeight / gridHeight > 0.21) {
+            $(".tournament-card_3nd").css("bottom", `calc(-1 * (${bottom}px))`)
+            $(".tournament-grid-wrapper").css("height", `${gridHeight + bottom}px`)
+        }
+
 
         let a = $(".tournament-grid__stage_semifinal .tournament-card:first-child").offset().top,
             b = $(".tournament-grid__stage_semifinal .tournament-card:last-child").offset().top
         
         $(".tournament-grid__stage_semifinal").css("--line-height", b - a)
-        
-        let gridHeight = $(".tournament-grid").innerHeight()
-        let thirdHeight = $(".tournament-card_3nd").innerHeight()
-        
-        if (thirdHeight / gridHeight > 0.21) {
-            $(".tournament-card_3nd").css("bottom", `calc(-1 * (${(thirdHeight / gridHeight - 0.21)*500}px + 1.25em))`)
-        }
 
     } catch {}
 }
 
 drawLine()
+
+function cardResize() {
+    let gap = 30;
+    let size = ($(".tournament-grid-wrapper").outerWidth() - 90 - 2 * gap) / 3;
+    if (size < 380) {
+        $(".tournament-card").css("width", '')
+        return
+    }
+    $(".tournament-card").css("width", `${size}px`)
+}
+
+cardResize()
+
+$(window).resize(cardResize)
 
 // function thirdCardResize() {
 //     if ($(window).innerWidth() < 768) {
